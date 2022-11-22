e344d3d5894317cd40a5baaa7152494553991578
OsanaiNaoto <220502@jc-21.jp>
2022-11-22 15:06:24 +0900

    1st

---
+++ kadai1/data_add.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>個人情報</title>
+    </head>
+
+    <body>
+        個人情報登録<br />
+        <br />
+        <form method="POST" action="data_add_check.php">
+            情報を入力してください。
+            <input type="text" name="id">
+        </form>
+    </body>
+</html>
