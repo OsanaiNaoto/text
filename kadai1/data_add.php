d7d3e8eb50aca9aba446d274c0ae091f72e23859
OsanaiNaoto <220502@jc-21.jp>
2022-11-30 15:29:26 +0900

    2nd

--- kadai1/data_add.php
+++ kadai1/data_add.php
@@ -9,8 +9,18 @@
         個人情報登録<br />
         <br />
         <form method="POST" action="data_add_check.php">
-            情報を入力してください。
-            <input type="text" name="id">
+            情報を入力してください。<br />
+            ID:<input type="text" name="id" style="width: 125px"><br />
+            名前:<input type="text" name="name" style="width: 125px"><br />
+            ふりがな:<input type="text" name="name_letter" style="width: 125px"><br />
+            郵便番号:<input type="text" name="address_num" style="width: 125px"><br />
+            住所:<input type="text" name="address" style="width: 200px"><br />
+            電話番号:<input type="text" name="num" style="width: 125px"><br />
+            Eメールアドレス:<input type="text" name="mail" style="width: 125px"><br />
+
+            <button type="button" onclick="history.back()">戻る</button>
+            <button type="submit">OK</button>
+
         </form>
     </body>
 </html>
