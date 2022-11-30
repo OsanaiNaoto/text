f05c882ea81fa63998b952a5b97a6c9f0e23cef7
OsanaiNaoto <220502@jc-21.jp>
2022-11-30 16:33:54 +0900

    3rd

--- kadai1/data_add.php
+++ kadai1/data_add.php
@@ -10,13 +10,15 @@
         <br />
         <form method="POST" action="data_add_check.php">
             情報を入力してください。<br />
-            ID:<input type="text" name="id" style="width: 125px"><br />
-            名前:<input type="text" name="name" style="width: 125px"><br />
-            ふりがな:<input type="text" name="name_letter" style="width: 125px"><br />
-            郵便番号:<input type="text" name="address_num" style="width: 125px"><br />
-            住所:<input type="text" name="address" style="width: 200px"><br />
-            電話番号:<input type="text" name="num" style="width: 125px"><br />
-            Eメールアドレス:<input type="text" name="mail" style="width: 125px"><br />
+            ID<br /><input type="text" name="id" style="width: 125px"><br />
+            名前<br /><input type="text" name="name" style="width: 125px"><br />
+            ふりがな<br /><input type="text" name="name_letter" style="width: 125px"><br />
+            郵便番号<br /><input type="text" name="address_num" style="width: 125px"><br />
+            住所<br /><input type="text" name="address" style="width: 200px"><br />
+            電話番号<br /><input type="text" name="phone_num" style="width: 125px"><br />
+            電話番号<br /><input type="text" name="phone_num" style="width: 125px"><br />
+            電話番号<br /><input type="text" name="phone_num" style="width: 125px"><br />
+            Eメールアドレス<br /><input type="text" name="mail" style="width: 125px"><br />
 
             <button type="button" onclick="history.back()">戻る</button>
             <button type="submit">OK</button>
---
+++ kadai1/data_add_check.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>個人情報</title>
+    </head>
+
+    <body>
+    <?php
+        
+        $id  = $_POST["id"];
+        $name= $_POST["name"];
+        $name_letter = $_POST["name_letter"];
+        $address_num = $_POST["address_num"];
+        $address = $_POST["address"];
+        $phone_num = $_POST["phone_num"];
+        $mail = $_POST["mail"];
+
+        $id = htmlspecialchars($id, ENT_QUOTES,"utf-8");
+        $name = htmlspecialchars($name, ENT_QUOTES,"utf-8");
+        $name_letter = htmlspecialchars($name_letter, ENT_QUOTES,"utf-8");
+        $address_num = htmlspecialchars($address_num, ENT_QUOTES,"utf-8");
+        $address = htmlspecialchars($address, ENT_QUOTES,"utf-8");
+        $phone_num = htmlspecialchars($phone_num, ENT_QUOTES,"utf-8");
+        $mail = htmlspecialchars($mail, ENT_QUOTES,"utf-8");
+
+        if($name == '') {
+            print '名前が入力されていません。<br />';
+        } else {
+            print '名前';
+            print $name;
+            print '<br />';
+        }
+
+        if($name_letter == '') {
+            print 'ふりがなが入力されていません。<br />';
+        } else {
+            print 'ふりがな';
+            print $name_letter;
+            print '<br />';
+        }
+
+        if($address_num == '') {
+            print '郵便番号が入力されていません。<br />';
+        } else {
+            print '郵便番号';
+            print $address_num;
+            print '<br />';
+        }
+
+        if($address == '') {
+            print '住所が入力されていません。<br />';
+        } else {
+            print '住所';
+            print $address;
+            print '<br />';
+        }
+
+        if($phone_num == '') {
+            print '電話番号が入力されていません。<br />';
+        } else {
+            print '電話番号';
+            print $phone_num;
+            print '<br />';
+        }
+
+        if($mail == '') {
+            print 'Eメールアドレスが入力されていません。<br />';
+        } else {
+            print 'Eメールアドレス';
+            print $mail;
+            print '<br />';
+        }
+
+
+        if($name==''||$name_letter==''||$address_num==''||$address==''||$phone_num==''||$mail=='') {
+            print '<form>';
+            print '<button type="button" onclick="history_back()">戻る</button>';
+            print '</form>';
+        } else {
+         print '<form method="post" action="staff_add_done.php">' ;
+         print '<input type="hid   den" name="name" value=" .$staff_name">';
+         print '<input type="hidden" name="pass" value=" .$staff_pass">';
+         print '<br />';
+         print '<input type="button" onclick="history.back()" value="戻る">';
+         print '<input type="submit" value="OK"?>';
+        }
+        ?>
+    
+    </body>
+</html>
